0db22b38f620730490dd81d244b602710cd1e883
Alex Kuzmenkov <aleksand.kuz@gmail.com>
2020-08-31 14:44:38 +0200

    firs commit

--- src/model/Model.java
+++ src/model/Model.java
@@ -21,7 +21,7 @@ public class Model {
             while (line != null) {
                 String a;
                 String[] question = line.split(";");
-                questions.put(Integer.parseInt(question[0]), new Questions(question[1], question[2], question[3], question[4], question[5]));
+//                questions.put(Integer.parseInt(question[0]), new Questions(question[1], question[2], question[3], question[4], question[5]));
 
                 line = reader.readLine();
 
